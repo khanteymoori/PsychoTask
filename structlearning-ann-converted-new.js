d77f851bf5802da63d2d04b0641e351ceb8b3d64
Alireza Khanteymoori <47894572+khanteymoori@users.noreply.github.com>
2024-12-23 21:18:05 +0100

    Update structlearning-ann-converted-new.js

--- structlearning-ann-converted-new.js
+++ structlearning-ann-converted-new.js
@@ -776,7 +776,7 @@ async function experimentInit() {
 console.log('Saving data...');
 
 // Your Google Apps Script Web App URL
-const webAppUrl = 'https://script.google.com/macros/s/AKfycbyjgm2XTsEWThPM6QJkPU-sQ8luHG1IrEA0zveDjE1uOVWrESzO4crDd47WRLMncJNW/exec'; // Replace with your deployed Apps Script URL
+const webAppUrl = 'https://script.google.com/macros/s/AKfycbzK2_U9ufXV0-5_lcczHebsXDJ6Iz0EKvnyaDbmIAyFBX7wv7ht9WD1Q2ym_Z1a2bF_/exec'; // Replace with your deployed Apps Script URL
 
 
 fetch(webAppUrl, {
